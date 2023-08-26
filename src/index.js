bfdd3851ac123c4ea6b8d37496b2e520ae929989
IlushinGL <ilushingl@yandex.ru>
2023-08-26 19:14:02 +0300

    refactor: удалить лишнее

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
+
+// Если нужно измерять производительность приложения:
+
 // import reportWebVitals from './reportWebVitals';
+
+// передайте функцию для регистрации результатов
+// например: reportWebVitals(console.log)
+// или отправте их на конечную точку аналитики.
+// Узнайте больше: https://bit.ly/CRA-vitals
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,8 +24,4 @@ root.render(
   </React.StrictMode>
 );
 
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+// reportWebVitals(console.log);
